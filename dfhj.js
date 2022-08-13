496af966601045e161e15b2b6208222a6201b02a
re-arun <re-arun@indicchain.com>
2022-08-13 17:34:23 +0530

    13 aug ,Final code before mergin with all screens

---
+++ dfhj.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const dfhj = () => {
+  return (
+    <View>
+      <Text>dfhj</Text>
+    </View>
+  )
+}
+
+export default dfhj
+
+const styles = StyleSheet.create({})
---
+++ jkdsfhjkd.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const jkdsfhjkd = () => {
+  return (
+    <View>
+      <Text>jkdsfhjkd</Text>
+    </View>
+  )
+}
+
+export default jkdsfhjkd
+
+const styles = StyleSheet.create({})
